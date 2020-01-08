afbe06bc6f70d515e7536ca05e816ac1b3b36dbe
João Carlos Lima e Silva <joaocarloslima@me.com>
2020-01-08 18:00:50 -0300

    crud de locais (editar, excluir e cadastrar)

--- adm/locais.php
+++ adm/locais.php
@@ -1,12 +1,42 @@
 <?php
-//buscar os locais cadastrados
 require_once "../global.php";
+include "../notificacao.php";
+
+//apagar
+if (isset($_POST["idApagar"])) {
+    $local = new Local();
+    $local->id = $_POST["idApagar"];
+    $local->apagar();
+}
+
+//editar
+if (isset($_POST["idEditar"]) && $_POST["idEditar"]!="") {
+    $local = new Local();
+    $local->id = $_POST["idEditar"];
+    $local->nome = $_POST["nome"];
+    $local->sigla = $_POST["sigla"];
+    $local->atualizar();
+}
+
+//inserir
+if (isset($_POST["nome"]) && $_POST["idEditar"]=="") {
+    $local = new Local();
+    $local->nome = $_POST["nome"];
+    $local->sigla = $_POST["sigla"];
+    $local->inserir();
+}
+
+//buscar os locais cadastrados
 $local = new Local();
 $locais = $local->buscarTodos();
 
 ?>
 
 <?php include "cabecalho.php" ?>
+<?php
+mostrarNotificacao('red');
+mostrarNotificacao('green');
+?>
 <br>
 <button class="ui button teal" id="btnNovo">
     <i class="icon plus"></i>
@@ -29,8 +59,8 @@ $locais = $local->buscarTodos();
                 <td>
                     <div class="ui small basic icon buttons">
                         <button class="ui button" data-tooltip="ver equipamentos do local"><i class="desktop icon"></i></button>
-                        <button class="ui button" data-tooltip="editar"><i class="pencil icon"></i></button>
-                        <button class="ui button" data-tooltip="apagar" onclick="excluir()"><i class="trash icon"></i></button>
+                        <button class="ui button" data-tooltip="editar" onclick="editar('<?= $local["id"] ?>', '<?= $local["nome"] ?>', '<?= $local["sigla"]?>')"><i class="pencil icon"></i></button>
+                        <button class="ui button" data-tooltip="apagar" onclick="excluir(<?= $local["id"] ?>)"><i class="trash icon"></i></button>
                     </div>
                 </td>
             </tr>
@@ -41,44 +71,49 @@ $locais = $local->buscarTodos();
 
 <!-- modal inserir-->
 <div class="ui modal" id="modalinserir">
-    <div class="header">Cadastrar Local</div>
+    <div class="header">Cadastro de Local</div>
     <div class="content">
-        <form class="ui form">
+        <form class="ui form" action="locais.php" method="POST">
+            <input type="hidden" name="idEditar" id="campo-id">
             <div class="two fields">
                 <div class="twelve wide field">
                     <label>Nome</label>
-                    <input type="text" name="nome" placeholder="nome do local">
+                    <input type="text" name="nome" placeholder="nome do local" id="campo-nome">
                 </div>
                 <div class="four wide field">
                     <label>Sigla</label>
-                    <input type="text" name="sigla" placeholder="sigla do local">
+                    <input type="text" name="sigla" placeholder="sigla do local" id="campo-sigla">
                 </div>
             </div>
-        </form>
-
-    </div>
-    <div class="actions">
-        <button class="ui cancel button">Cancelar</button>
-        <button class="ui teal approve button">Salvar</button>
-    </div>
+            
+        </div>
+        <div class="actions">
+            <div class="ui cancel button">Cancelar</div>
+            <button type="submit" class="ui teal approve button">Salvar</button>
+        </div>
+    </form>
 </div>
 
 <!-- modal exluir-->
 <div class="ui basic modal" id="modalexcluir">
     <div class="ui icon header">
-        <i class="trash icon"></i> Apagar usuário
+        <i class="trash icon"></i> Apagar local
     </div>
     <div class="content">
-        <p>Tem certeza que deseja apagar esse usuário? Todos os dados relacionados a ele serão perdidos. Essa ação não poderá ser desfeita</p>
+        <p>Tem certeza que deseja apagar esse local? Todos os dados relacionados a ele serão perdidos. Essa ação não poderá ser desfeita</p>
     </div>
-    <div class="actions">
-        <div class="ui green basic cancel inverted button">
-            <i class="remove icon"></i> Não Apagar
+    <form action="locais.php" method="POST">
+        <input type="hidden" name="idApagar" id="id-apagar">
+        <div class="actions">
+            <div class="ui green basic cancel inverted button">
+                <i class="remove icon"></i> Não Apagar
+            </div>
+
+            <button class="ui red ok inverted button">
+                <i class="checkmark icon"></i> Apagar
+            </button>
         </div>
-        <div class="ui red ok inverted button">
-            <i class="checkmark icon"></i> Apagar
-        </div>
-    </div>
+    </form>
 </div>
 
 
@@ -86,11 +121,22 @@ $locais = $local->buscarTodos();
 
 <script type="text/javascript">
     $('#btnNovo').on("click", function() {
+        $('#campo-id').val("");
+        $('#campo-nome').val("");
+        $('#campo-sigla').val("");
         $('#modalinserir').modal('show');
     });
 
-    function excluir() {
+    function excluir($id) {
+        $('#id-apagar').val($id);
         $('#modalexcluir').modal('show');
+    }
+
+    function editar($id, $nome, $sigla){
+        $('#campo-id').val($id);
+        $('#campo-nome').val($nome);
+        $('#campo-sigla').val($sigla);
+        $('#modalinserir').modal('show');
     }
 
     $('.dropdown').dropdown();
--- classes/Local.php
+++ classes/Local.php
@@ -12,4 +12,45 @@ class Local{
 		return $stmt->fetchAll();
 	}
 
+	public function apagar() {
+		$conexao = Conexao::pegarConexao();
+		$query = "DELETE FROM locais WHERE id=:id";
+		$stmt = $conexao->prepare($query);
+		$stmt->bindValue(":id", $this->id);
+		try {
+			$stmt->execute();
+			$_SESSION["green"] = "Local apagado com sucesso";
+		} catch (\Throwable $e) {
+			$_SESSION["red"] = "Erro ao apagar local. <br><br>[$e]";
+		}
+	}
+
+	public function inserir() {
+		$conexao = Conexao::pegarConexao();
+		$query = "INSERT INTO locais (id, nome, sigla) VALUES (0, :nome, :sigla)";
+		$stmt = $conexao->prepare($query);
+		$stmt->bindValue(":nome", $this->nome);
+		$stmt->bindValue(":sigla", $this->sigla);
+		try {
+			$stmt->execute();
+			$_SESSION["green"] = "Local cadastrado com sucesso";
+		} catch (\Throwable $e) {
+			$_SESSION["red"] = "Erro ao cadastrar local. <br><br>[$e]";
+		}
+	}
+
+	public function atualizar() {
+		$conexao = Conexao::pegarConexao();
+		$query = "UPDATE locais SET nome=:nome, sigla=:sigla WHERE id=:id";
+		$stmt = $conexao->prepare($query);
+		$stmt->bindValue(":id", $this->id);
+		$stmt->bindValue(":nome", $this->nome);
+		$stmt->bindValue(":sigla", $this->sigla);
+		try {
+			$stmt->execute();
+			$_SESSION["green"] = "Local alterado com sucesso";
+		} catch (\Throwable $e) {
+			$_SESSION["red"] = "Erro ao alterar local. <br><br>[$e]";
+		}
+	}
 }
