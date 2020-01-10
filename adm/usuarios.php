96451422577cb129acd3db3db41e24f738054cf2
Joao Pedro <joaopedrowatson@hotmail.com>
2020-01-10 12:23:11 -0300

    Crud completo terminado

--- adm/usuarios.php
+++ adm/usuarios.php
@@ -1,13 +1,53 @@
 <?php
+include "../notificacao.php";
 require_once "../global.php";
 require_once "../classes/Usuario.php";
 
+// Listagem de usuarios
 $usuario = new Usuario();
 $listaUsuarios = $usuario->buscarTodos();
+
+// Apagar usuario
+if (isset($_POST["idApagar"])){
+    $usuario = new Usuario();
+    $usuario->id = $_POST["idApagar"];
+
+    $usuario->apagar();
+}
+
+// Inserir
+if (isset($_POST["matricula"]) && $_POST["idEditar"] == ""){
+    $usuario = new Usuario();
+    $usuario->matricula = $_POST["matricula"];
+    $usuario->nome = $_POST["nome"];
+    $usuario->email = $_POST["email"];
+    $usuario->perfil = $_POST["perfil"];
+    $usuario->senha = $_POST["senha"];
+
+    $usuario->inserir();
+}
+
+// Atualizar
+if(isset($_POST["idEditar"]) && $_POST["idEditar"] != ""){
+    $usuario = new Usuario();
+    $usuario->id = $_POST["idEditar"];
+    $usuario->matricula = $_POST["matricula"];
+    $usuario->nome = $_POST["nome"];
+    $usuario->email = $_POST["email"];
+    $usuario->perfil = $_POST["perfil"];
+    $usuario->senha = $_POST["senha"];
+
+    $usuario->atualizar();
+}
 
 ?>
 
 <?php include "cabecalho.php"; ?>
+
+<?php
+    mostrarNotificacao('red');
+    mostrarNotificacao('green');
+?>
 
 <br>
 <button class="ui button teal" id="btnNovo">
@@ -15,62 +55,65 @@ $listaUsuarios = $usuario->buscarTodos();
   Novo usuário
 </button>
 <table class="ui teal celled padded table datatable" id="datatable">
-    <thead>
-        <th class="center aligned">#</th>
-        <th>Matrícula</th>
-        <th>Nome</th>
-        <th>Email</th>
-        <th>Perfil</th>
-        <th>Ações</th>
-        </tr>
+  <thead>
+    <th class="center aligned">#</th>
+    <th>Matrícula</th>
+    <th>Nome</th>
+    <th>Email</th>
+    <th>Perfil</th>
+    <th>Ações</th>
+    </tr>
   </thead>
 
-    <tbody>
-        <?php foreach ($listaUsuarios as $usuario) : ?>
-            <tr>
-                <td class="center aligned"> IMAGE <img class="ui avatar image" src="../fotos/<?= $usuario['matricula']?>.jpg"></td>
-                <td class="center aligned"><?= $usuario['id'] ?></td>
-                <td><?= $usuario['matricula']?></td>
-                <td class="single line"><?= $usuario['nome']?></td>
-                <td><?= $usuario['email']?></td>
-                <td><?= $usuario['matricula'] = 1?'Administrador':'Usuario'?></td>
-                <td>
-                <div class="ui small basic icon buttons">
-                    <button class="ui button"><i class="comment icon"></i></button>
-                    <button class="ui button"><i class="pencil icon"></i></button>
-                    <button class="ui red button" onclick="excluir()"><i class="trash icon"></i></button>
-                </div>
-                </td>
-            </tr>
-        <?php endforeach ?>
-    </tbody>
+  <tbody>
+    <?php foreach ($listaUsuarios as $usuario) : ?>
+      <tr>
+        <td class="center aligned"><img class="ui avatar image" src="../fotos/<?= is_file("../fotos/".(string)$usuario['matricula'])."jpg"?$usuario['matricula'] :'semFoto' ?>.jpg"></td>
+        <td><?= $usuario['matricula'] ?></td>
+        <td class="single line"><?= $usuario['nome'] ?></td>
+        <td><?= $usuario['email'] ?></td>
+        <td><?=$usuario['perfil'] = 1 ? 'Administrador' : 'Usuario' ?></td>
+        <td>
+          <div class="ui small basic icon buttons">
+            <button class="ui button"><i class="comment icon"></i></button>
+            <button class="ui button"  onclick="editar('<?=$usuario['id'] ?>', '<?=$usuario['matricula'] ?>', 
+                '<?=$usuario['nome'] ?>', '<?=$usuario['email'] ?>', '<?=$usuario['perfil'] ?>', '<?=$usuario['senha'] ?>')">
+                <i class="pencil icon"></i>
+            </button>
+            <button class="ui red button" onclick="excluir(<?= $usuario['id'] ?>)"> <i class="trash icon"></i></button>
+          </div>
+        </td>
+      </tr>
+    <?php endforeach ?>
+  </tbody>
 </table>
 
 
 <!-- modal inserir-->
 <div class="ui modal" id="modalinserir">
-  <div class="header">Cadastrar Usuário</div>
+  <div class="header">Cadastrar de Usuário</div>
   <div class="content">
-    <form class="ui form">
+    <form class="ui form" action="usuarios.php" method="POST">
+      <input type="hidden" name='idEditar' id="campo-id">
       <div class="two fields">
         <div class="field">
           <label>Matrícula</label>
-          <input type="number" name="">
+          <input type="number" name="matricula" id='campo-matricula'>
         </div>
         <div class="field">
           <label>Perfil</label>
           <div class="ui fluid selection dropdown">
-            <input type="hidden" name="perfil">
+            <input type="hidden" name="perfil" id='campo-perfil'>
             <i class="dropdown icon"></i>
             <div class="default text">selecione</div>
             <div class="menu">
-              <div class="item" data-value="jenny" data-text="Usuário">
+              <div class="item" data-value = 0 data-text="Usuário">
                 <i class="ui avatar icon user outline"></i> Usuário
               </div>
-              <div class="item" data-value="elliot" data-text="Técnico">
+              <div class="item" data-value = 2 data-text="Técnico">
                 <i class="ui avatar icon user"></i> Técnico
               </div>
-              <div class="item" data-value="stevie" data-text="Administrador">
+              <div class="item" data-value = 1 data-text="Administrador">
                 <i class="ui avatar icon user secret"></i> Administrador
               </div>
             </div>
@@ -79,25 +122,25 @@ $listaUsuarios = $usuario->buscarTodos();
       </div>
       <div class="field">
         <label>Nome</label>
-        <input type="text" name="" placeholder="nome completo">
+        <input type="text" name="nome" placeholder="nome completo" id='campo-nome'>
       </div>
       <div class="two fields">
         <div class="field">
-          <label>E-mail</label>
-          <input type="email" name="" placeholder="e-mail de contato">
+            <label>E-mail</label>
+            <input type="email" name="email" placeholder="e-mail de contato" id='campo-email'>
         </div>
         <div class="field">
           <label>Senha</label>
-          <input type="password" name="" placeholder="senha de acesso">
+          <input type="password" name="senha" placeholder="senha de acesso" id='campo-senha'>
         </div>
-      </div>
-    </form>
+         </div>
 
-  </div>
-  <div class="actions">
-    <button class="ui cancel button">Cancelar</button>
-    <button class="ui teal approve button">Salvar</button>
-  </div>
+        </div>
+        <div class="actions">
+            <div class="ui cancel button">Cancelar</div>
+            <button type="submit" class="ui teal approve button">Salvar</button>
+        </div>
+    </form>
 </div>
 
 <!-- modal exluir-->
@@ -105,29 +148,52 @@ $listaUsuarios = $usuario->buscarTodos();
   <div class="ui icon header">
     <i class="trash icon"></i> Apagar usuário
   </div>
+
   <div class="content">
     <p>Tem certeza que deseja apagar esse usuário? Todos os dados relacionados a ele serão perdidos. Essa ação não poderá ser desfeita</p>
   </div>
-  <div class="actions">
-    <div class="ui green basic cancel inverted button">
-      <i class="remove icon"></i> Não Apagar
+
+  <form action="usuarios.php" method="POST">  
+    <input type="hidden" name="idApagar" id="id-apagar">
+    <div class="actions">
+      <div class="ui green basic cancel inverted button">
+        <i class="remove icon"></i> Não Apagar
+      </div>
+      <button action="submit" class="ui red ok inverted button">
+        <i class="checkmark icon"></i> Apagar
+      </button>
     </div>
-    <div class="ui red ok inverted button">
-      <i class="checkmark icon"></i> Apagar
-    </div>
-  </div>
+  </form>
 </div>
 
 
 <?php include "rodape.php" ?>
 
 <script type="text/javascript">
-  $('#btnNovo').on("click", function() {
+
+    $('#btnNovo').on("click", function() {
+    $('#campo-id').val("");
+    $('#campo-matricula').val("");
+    $('#campo-nome').val("");
+    $('#campo-email').val("");
+    $('#campo-perfil').val("Selecione");
+    $('#campo-senha').val("");
     $('#modalinserir').modal('show');
   });
 
-  function excluir() {
-    $('#modalexcluir').modal('show');
+  function editar($id, $matricula, $nome, $email, $perfil, $senha){
+    $('#campo-id').val($id);
+    $('#campo-matricula').val($matricula);
+    $('#campo-nome').val($nome);
+    $('#campo-email').val($email);
+    $('#campo-perfil').val("Selecione");
+    $('#campo-senha').val($senha);
+    $('#modalinserir').modal("show");
+  }
+
+  function excluir($id) {
+      $('#id-apagar').val($id);
+      $('#modalexcluir').modal('show');
   }
 
   $('.dropdown').dropdown();
